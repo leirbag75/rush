56825541335260d082ff407975caa699eee879fd
Gabriel Johnson <leirbag75@gmail.com>
2021-04-04 13:59:12 -0700

    Refactor test battle code

--- test/test-battle.lisp
+++ test/test-battle.lisp
@@ -16,17 +16,27 @@
   ((name :reader name
          :initform (format nil "Mock ~A" (incf *mock-combatant-number*)))))
 
+(defun make-battle-with-combatants (&rest combatants)
+  (make-instance 'battle :combatants combatants))
+
+(defparameter *weak-damage* 10)
+
+(defun inflict-weak-damage (battle combatant)
+  (inflict-damage battle combatant *weak-damage*))
+
+(defun make-weak-damage-event (combatant)
+  (make-instance 'damage-infliction
+                 :target combatant
+                 :amount *weak-damage*))
+
 (deftest battle-base-case (test-battle)
-  (let ((battle (make-instance 'battle)))
+  (let ((battle (make-battle-with-combatants)))
     (assert-events-match '()
                          (next-events battle))))
 
 (deftest battle-single-event (test-battle)
   (let* ((combatant (make-instance 'mock-combatant))
-         (battle (make-instance 'battle
-                                :combatants (list combatant))))
-    (inflict-damage battle combatant 10)
-    (assert-events-match (list (make-instance 'damage-infliction
-                                              :target combatant
-                                              :amount 10))
+         (battle (make-battle-with-combatants combatant)))
+    (inflict-weak-damage battle combatant)
+    (assert-events-match (list (make-weak-damage-event combatant))
                          (next-events battle))))
