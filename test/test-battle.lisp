2a35aee8ed14092740ba3f65e67a9f81a3890441
Gabriel Johnson <leirbag75@gmail.com>
2021-04-06 09:08:03 -0700

    Remove unnecessary tests from battle

--- test/test-battle.lisp
+++ test/test-battle.lisp
@@ -15,18 +15,8 @@
 (defun make-battle-with-combatants (&rest combatants)
   (make-instance 'battle :combatants combatants))
 
-(defparameter *weak-damage* 10)
-
-(defun inflict-weak-damage (battle combatant)
-  (inflict-damage battle combatant *weak-damage*))
-
 (defun half-kill (battle combatant)
   (inflict-damage battle combatant (floor *mock-combatant-max-hp* 2)))
-
-(defun make-weak-damage-event (combatant)
-  (make-instance 'damage-infliction
-                 :target combatant
-                 :amount *weak-damage*))
 
 (defun make-half-kill-event (combatant)
   (make-instance 'damage-infliction
@@ -41,36 +31,6 @@
 
 (defun resulting-events (battle)
   (next-events battle))
-
-(deftest battle-base-case (test-battle)
-  (let ((battle (make-battle-with-combatants)))
-    (assert-events-match '()
-                         (resulting-events battle))))
-
-(deftest battle-single-event (test-battle)
-  (let* ((combatant (make-mock-combatant))
-         (battle (make-battle-with-combatants combatant)))
-    (inflict-weak-damage battle combatant)
-    (assert-events-match (list (make-weak-damage-event combatant))
-                         (resulting-events battle))))
-
-(deftest battle-discard-events (test-battle)
-  (let* ((combatant (make-mock-combatant))
-         (battle (make-battle-with-combatants combatant)))
-    (inflict-weak-damage battle combatant)
-    (resulting-events battle)
-    (assert-events-match '()
-                         (resulting-events battle))))
-
-(deftest battle-multiple-events (test-battle)
-  (let* ((combatant (make-mock-combatant))
-         (combatant2 (make-mock-combatant))
-         (battle (make-battle-with-combatants combatant combatant2)))
-    (inflict-weak-damage battle combatant)
-    (inflict-weak-damage battle combatant2)
-    (assert-events-match (list (make-weak-damage-event combatant)
-                               (make-weak-damage-event combatant2))
-                         (resulting-events battle))))
 
 (deftest battle-death (test-battle)
   (let* ((combatant (make-mock-combatant))
