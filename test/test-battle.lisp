100e819c98d452b3793064379e2aa22306ae1ebb
Gabriel Johnson <leirbag75@gmail.com>
2021-04-18 17:04:04 -0700

    DRY out test code

--- test/test-battle.lisp
+++ test/test-battle.lisp
@@ -18,19 +18,23 @@
 (defun make-mock-combatant ()
   (make-instance 'mock-combatant))
 
+(defun make-test-battle ()
+  (let* ((combatant1 (make-instance 'mock-combatant))
+         (combatant2 (make-instance 'mock-combatant))
+         (battle (make-battle-with-combatants combatant1 combatant2)))
+    (values battle combatant1 combatant2)))
+
 (deftest subtracts-hp (test-battle)
-  (let* ((combatant (make-mock-combatant))
-         (remaining-hp (- *mock-combatant-max-hp* 10))
-         (battle (make-battle-with-combatants combatant)))
-    (inflict-damage battle combatant 10)
-    (unless (eql (remaining-hp battle combatant) remaining-hp)
-      (error 'objects-dont-match
-             :expected remaining-hp
-             :actual (remaining-hp battle combatant)))))
+  (multiple-value-bind (battle combatant) (make-test-battle)
+    (let ((remaining-hp (- *mock-combatant-max-hp* 10)))
+      (inflict-damage battle combatant 10)
+      (unless (eql (remaining-hp battle combatant) remaining-hp)
+        (error 'objects-dont-match
+               :expected remaining-hp
+               :actual (remaining-hp battle combatant))))))
 
 (deftest keeps-hp-at-least-0 (test-battle)
-  (let* ((combatant (make-mock-combatant))
-         (battle (make-battle-with-combatants combatant)))
+  (multiple-value-bind (battle combatant) (make-test-battle)
     (inflict-damage battle combatant (* 2 *mock-combatant-max-hp*))
     (unless (eql (remaining-hp battle combatant) 0)
       (error 'objects-dont-match
@@ -38,8 +42,7 @@
              :actual (remaining-hp battle combatant)))))
 
 (deftest adds-damage-infliction-event (test-battle)
-  (let* ((combatant (make-mock-combatant))
-         (battle (make-battle-with-combatants combatant)))
+  (multiple-value-bind (battle combatant) (make-test-battle)
     (inflict-damage battle combatant 10)
     (assert-events-match battle
                          (make-instance 'damage-infliction
@@ -47,19 +50,17 @@
                                         :amount 10))))
 
 (deftest adds-hp (test-battle)
-  (let* ((combatant (make-mock-combatant))
-         (remaining-hp (* *mock-combatant-max-hp* 3/4))
-         (battle (make-battle-with-combatants combatant)))
-    (inflict-damage battle combatant (/ *mock-combatant-max-hp* 2))
-    (heal-damage battle combatant (/ *mock-combatant-max-hp* 4))
-    (unless (eql (remaining-hp battle combatant) remaining-hp)
-      (error 'objects-dont-match
-             :expected remaining-hp
-             :actual (remaining-hp battle combatant)))))
+  (multiple-value-bind (battle combatant) (make-test-battle)
+    (let ((remaining-hp (* *mock-combatant-max-hp* 3/4)))
+      (inflict-damage battle combatant (/ *mock-combatant-max-hp* 2))
+      (heal-damage battle combatant (/ *mock-combatant-max-hp* 4))
+      (unless (eql (remaining-hp battle combatant) remaining-hp)
+        (error 'objects-dont-match
+               :expected remaining-hp
+               :actual (remaining-hp battle combatant))))))
 
 (deftest keeps-hp-at-most-max (test-battle)
-  (let* ((combatant (make-mock-combatant))
-         (battle (make-battle-with-combatants combatant)))
+  (multiple-value-bind (battle combatant) (make-test-battle)
     (heal-damage battle combatant 10)
     (unless (eql (remaining-hp battle combatant) *mock-combatant-max-hp*)
       (error 'objects-dont-match
@@ -67,8 +68,7 @@
              :actual (remaining-hp battle combatant)))))
 
 (deftest adds-damage-heal-event (test-battle)
-  (let* ((combatant (make-mock-combatant))
-         (battle (make-battle-with-combatants combatant)))
+  (multiple-value-bind (battle combatant) (make-test-battle)
     (heal-damage battle combatant 10)
     (assert-events-match battle
                          (make-instance 'damage-heal
@@ -76,8 +76,7 @@
                                         :amount 10))))
 
 (deftest battle-death (test-battle)
-  (let* ((combatant (make-mock-combatant))
-         (battle (make-battle-with-combatants combatant)))
+  (multiple-value-bind (battle combatant) (make-test-battle)
     (inflict-damage battle combatant *mock-combatant-max-hp*)
     (assert-events-match battle
                          (make-instance 'damage-infliction
@@ -93,23 +92,21 @@
   (funcall (body move) battle combatant target))
 
 (deftest should-add-use-move-event (test-battle)
-  (let* ((combatant (make-mock-combatant))
-         (move (make-instance 'mock-move
-                              :body (lambda (&rest args)
-                                      (declare (ignore args)))))
-         (battle (make-battle-with-combatants combatant)))
-    (input-moves battle combatant (list (cons move combatant)))
-    (assert-events-match battle
-                         (make-instance 'move-use
-                                        :move move
-                                        :user combatant))))
+  (multiple-value-bind (battle combatant) (make-test-battle)
+    (let* ((move (make-instance 'mock-move
+                                :body (lambda (&rest args)
+                                        (declare (ignore args))))))
+      (input-moves battle combatant (list (cons move combatant)))
+      (assert-events-match battle
+                           (make-instance 'move-use
+                                          :move move
+                                          :user combatant)))))
 
 (deftest should-call-perform-move (test-battle)
   (should-be-evaluated (-->this) ("perform-move not called")
-    (let* ((combatant (make-mock-combatant))
-           (move (make-instance 'mock-move
-                                :body (lambda (&rest args)
-                                        (declare (ignore args))
-                                        -->this)))
-           (battle (make-battle-with-combatants combatant)))
-      (input-moves battle combatant (list (cons move combatant))))))
+    (multiple-value-bind (battle combatant) (make-test-battle)
+      (let ((move (make-instance 'mock-move
+                                 :body (lambda (&rest args)
+                                         (declare (ignore args))
+                                         -->this))))
+        (input-moves battle combatant (list (cons move combatant)))))))
