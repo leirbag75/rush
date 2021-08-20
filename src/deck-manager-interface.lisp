5c0d3caa76986a85eee357de714c0bf6344f3368
Gabriel Johnson <leirbag75@gmail.com>
2021-08-19 18:49:40 -0700

    Remove use-card from deck manager interface

--- src/deck-manager-interface.lisp
+++ src/deck-manager-interface.lisp
@@ -3,11 +3,6 @@
 
 (defgeneric shuffle-deck (deck-manager))
 
-;; With use-card, the card must be in the user's hand. With
-;; discard-card, any arbitrary card can be discarded.
-
-(defgeneric use-card (deck-manager card))
-
 (defgeneric discard-card (deck-manager card))
 
 (defgeneric hand (deck-manager))
