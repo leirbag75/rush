e6f20bb560258dfe93f9dfc347009048784e38e1
Gabriel Johnson <leirbag75@gmail.com>
2021-08-28 18:25:50 -0700

    Implement preempt-event

--- src/event-accumulator.lisp
+++ src/event-accumulator.lisp
@@ -17,3 +17,21 @@
         (substitute (make-instance 'negated-event :event event)
                     event
                     (events event-accumulator))))
+
+(defmethod preempt-event ((event-accumulator event-accumulator)
+                          preempted-event
+                          preempting-event)
+  (setf (events event-accumulator)
+        ;; Sorry to write something like this, but this was the most
+        ;; obvious way for me to do it... (Basically, it's using the
+        ;; list monad)
+        (reduce #'nconc
+                (events event-accumulator)
+                :from-end t
+                :key (lambda (event)
+                       ;; Since the event list is reversed before
+                       ;; being returned, the preempted-event comes
+                       ;; *first*
+                       (if (eql event preempted-event)
+                           (list preempted-event preempting-event)
+                           (list event))))))
--- test/test-event-accumulator.lisp
+++ test/test-event-accumulator.lisp
@@ -25,3 +25,11 @@
     (assert-events-match event-accumulator
                          (make-instance 'negated-event
                                         :event 1))))
+
+(deftest adds-preempting-event-before-preempted (test-event-accumulator)
+  (let ((event-accumulator (make-instance 'event-accumulator)))
+    (add-event event-accumulator 1)
+    (add-event event-accumulator 3)
+    (preempt-event event-accumulator 3 2)
+    (assert-events-match event-accumulator
+                         1 2 3)))
